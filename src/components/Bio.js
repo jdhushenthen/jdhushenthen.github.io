9e19319c212f69b339803c94b3e53121e732e711
Janahan Dhushenthen <jdhushenthen@rogers.com>
2021-09-04 13:45:12 -0400

    Updated bio and resume

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,10 +8,10 @@ export const Bio = () => {
             <p className="line"> Previously, I've worked at... </p>
 
             <ul className="job-list">
-                <li className="line"> <i>BlackBerry QNX</i> - reproducing and debugging customer issues with QNX products </li>
+                <li className="line"> <i>BlackBerry QNX</i> - reproducing and debugging customer issues with the QNX Software Development Platform </li>
                 <li className="line"> <i>Intel</i> - automating the hardware tests on the FPGA Emulation Platforms </li>
-                <li className="line"> <i>Statistics Canada</i> - developing a software package that implements the CART algorithm </li>
-                <li className="line"> <i>CAPREIT</i> - consolidating, updating and entering employee data </li>
+                <li className="line"> <i>Statistics Canada</i> - developing a software package that implements a variant of the CART algorithm </li>
+                <li className="line"> <i>CAPREIT</i> - consolidating and cleansing employee data in Active Directory </li>
             </ul>
 
             <p className="line"> I’m currently looking for full-time opportunities for 2022. </p>
